924656172a2336fad324747dd21b9ed870b25f4c
Andreas Reich <r_andreas2@web.de>
2020-05-08 00:28:53 +0200

    Using now shaderc's include resolve mechanism instead of my own Fixes #17

--- src/wgpu_utils/shader.rs
+++ src/wgpu_utils/shader.rs
@@ -1,97 +1,12 @@
 use notify::Watcher;
-use regex::Regex;
 use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
-pub enum ShaderStage {
-    Vertex,
-    Fragment,
-    Compute,
-}
-
 // All entry points need to have this name.
 // (could make customizable, but forcing this has perks as well)
 pub const SHADER_ENTRY_POINT_NAME: &str = "main";
-
-fn compile_glsl(glsl_code: &str, identifier: &str, stage: ShaderStage) -> Result<Vec<u32>, ()> {
-    let kind = match stage {
-        ShaderStage::Vertex => shaderc::ShaderKind::Vertex,
-        ShaderStage::Fragment => shaderc::ShaderKind::Fragment,
-        ShaderStage::Compute => shaderc::ShaderKind::Compute,
-    };
-
-    let mut compiler = shaderc::Compiler::new().unwrap();
-    let mut options = shaderc::CompileOptions::new().unwrap();
-    //options.set_hlsl_io_mapping(true);
-    options.set_warnings_as_errors();
-    options.set_target_env(shaderc::TargetEnv::Vulkan, 0);
-    // options.set_optimization_level(shaderc::OptimizationLevel::Zero);
-    // options.set_include_callback // TODO: Use this (didn't notice this option)
-    match compiler.compile_into_spirv(glsl_code, kind, identifier, SHADER_ENTRY_POINT_NAME, Some(&options)) {
-        Ok(compile_result) => {
-            if compile_result.get_num_warnings() > 0 {
-                warn!("warnings when compiling {}:\n{}", identifier, compile_result.get_warning_messages());
-            }
-
-            match wgpu::read_spirv(std::io::Cursor::new(&compile_result.as_binary_u8())) {
-                Ok(spirv) => Ok(spirv),
-                Err(io_error) => {
-                    error!("Compilation succeeded, but wgpu::read_spirv failed: {}", io_error);
-                    Err(())
-                }
-            }
-        }
-        Err(compile_error) => {
-            error!("{}", compile_error);
-            Err(())
-        }
-    }
-}
-
-fn load_glsl_and_resolve_includes(path: &Path) -> Result<String, ()> {
-    match std::fs::read_to_string(&path) {
-        Ok(glsl_code) => {
-            lazy_static! {
-                static ref INCLUDE_REGEX: Regex = Regex::new(r#"^\s*#\s*include\s+[<"](?P<file>.*)[>"]"#).unwrap();
-            }
-
-            let mut expanded_code = Vec::new();
-            for (line_number, line) in glsl_code.lines().enumerate() {
-                match INCLUDE_REGEX.captures(line) {
-                    Some(captures) => {
-                        expanded_code.push(format!("#line {}", 1));
-                        let included_file = captures
-                            .name("file")
-                            .expect(&format!(
-                                "Invalid glsl include line in \"{:?}\" line {}, (couldn't find \"file\"):\n\t{}",
-                                path, line_number, line,
-                            ))
-                            .as_str();
-                        match load_glsl_and_resolve_includes(&path.parent().unwrap().join(included_file)) {
-                            Ok(included_code) => expanded_code.push(included_code),
-                            Err(()) => {
-                                error!("Failed to process include \"{:?}\"line {}:\n\t{}", path, line_number, line);
-                                return Err(());
-                            }
-                        }
-                        expanded_code.push(format!("#line {}", line_number + 2));
-                    }
-                    None => {
-                        expanded_code.push(line.to_string());
-                    }
-                }
-            }
-
-            Ok(expanded_code.join("\n"))
-        }
-        Err(err) => {
-            error!("Failed to read shader file \"{:?}\": {}", path, err);
-            Err(())
-        }
-    }
-}
 
 pub struct ShaderDirectory {
     #[allow(dead_code)]
@@ -127,18 +42,64 @@ impl ShaderDirectory {
     pub fn load_shader_module(&self, device: &wgpu::Device, relative_filename: &Path) -> Result<wgpu::ShaderModule, ()> {
         let path = self.directory.join(relative_filename);
 
-        let shader_stage = match path.extension().and_then(OsStr::to_str) {
-            Some("frag") => ShaderStage::Fragment,
-            Some("vert") => ShaderStage::Vertex,
-            Some("comp") => ShaderStage::Compute,
+        let kind = match path.extension().and_then(OsStr::to_str) {
+            Some("frag") => shaderc::ShaderKind::Fragment,
+            Some("vert") => shaderc::ShaderKind::Vertex,
+            Some("comp") => shaderc::ShaderKind::Compute,
             _ => {
                 error!("Did not recognize file extension for shader file \"{:?}\"", path);
                 return Err(());
             }
         };
 
-        let glsl_code = load_glsl_and_resolve_includes(&path)?;
-        let spirv = compile_glsl(&glsl_code, &relative_filename.to_str().unwrap(), shader_stage)?;
+        let glsl_code = match std::fs::read_to_string(&path) {
+            Ok(glsl_code) => glsl_code,
+            Err(err) => {
+                error!("Failed to read shader file \"{:?}\": {}", path, err);
+                return Err(());
+            }
+        };
+
+        let spirv = {
+            let mut compiler = shaderc::Compiler::new().unwrap();
+            let mut options = shaderc::CompileOptions::new().unwrap();
+            //options.set_hlsl_io_mapping(true);
+            options.set_warnings_as_errors();
+            options.set_target_env(shaderc::TargetEnv::Vulkan, 0);
+            options.set_optimization_level(shaderc::OptimizationLevel::Performance);
+            // options.set_optimization_level(shaderc::OptimizationLevel::Zero); // Useful for debugging/inspecting, e.g. via RenderDoc
+
+            options.set_include_callback(|name, ty, source_file, depth| {
+                let path = self.directory.join(name);
+                match std::fs::read_to_string(&path) {
+                    Ok(glsl_code) => Ok(shaderc::ResolvedInclude {
+                        resolved_name: String::from(name),
+                        content: glsl_code,
+                    }),
+                    Err(err) => Err(format!("Failed to resolve include to {} in {}: {}", name, source_file, err)),
+                }
+            });
+
+            match compiler.compile_into_spirv(&glsl_code, kind, path.to_str().unwrap(), SHADER_ENTRY_POINT_NAME, Some(&options)) {
+                Ok(compile_result) => {
+                    if compile_result.get_num_warnings() > 0 {
+                        warn!("warnings when compiling {:?}:\n{}", path, compile_result.get_warning_messages());
+                    }
+
+                    match wgpu::read_spirv(std::io::Cursor::new(&compile_result.as_binary_u8())) {
+                        Ok(spirv) => spirv,
+                        Err(io_error) => {
+                            error!("Compilation succeeded, but wgpu::read_spirv failed: {}", io_error);
+                            return Err(());
+                        }
+                    }
+                }
+                Err(compile_error) => {
+                    error!("{}", compile_error);
+                    return Err(());
+                }
+            }
+        };
 
         // Write out the spirv shader for debugging purposes
         // {
