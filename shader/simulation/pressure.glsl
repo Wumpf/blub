ef1518a0dee12c3927497a26ccba11a971063058
Andreas Reich <r_andreas2@web.de>
2020-07-26 18:06:12 +0200

    Fix code duplication in pressure solver

--- shader/simulation/pressure.glsl
+++ shader/simulation/pressure.glsl
@@ -20,3 +20,50 @@ layout(push_constant) uniform PushConstants_ {
     uint SourceBufferSize; // The size of the source buffer
 }
 PushConstants;
+
+// Result of multiplication with coefficient matrix with a texture at gridCoord.
+// Only call if gridCoord is a fluid position!
+float MultiplyWithCoefficientMatrix(ivec3 gridCoord, texture3D texture, float valueAtGridCoord) {
+    float result = 0.0;
+    float markerX0 = texelFetch(MarkerVolume, gridCoord - ivec3(1, 0, 0), 0).x;
+    float markerX1 = texelFetch(MarkerVolume, gridCoord + ivec3(1, 0, 0), 0).x;
+    float markerY0 = texelFetch(MarkerVolume, gridCoord - ivec3(0, 1, 0), 0).x;
+    float markerY1 = texelFetch(MarkerVolume, gridCoord + ivec3(0, 1, 0), 0).x;
+    float markerZ0 = texelFetch(MarkerVolume, gridCoord - ivec3(0, 0, 1), 0).x;
+    float markerZ1 = texelFetch(MarkerVolume, gridCoord + ivec3(0, 0, 1), 0).x;
+
+    // This is the diagonal value of matrix A!
+    float numNonSolidNeighbors = 0.0;
+    numNonSolidNeighbors += abs(markerX0); // float(markerX0 != CELL_SOLID);
+    numNonSolidNeighbors += abs(markerX1); // float(markerX1 != CELL_SOLID);
+    numNonSolidNeighbors += abs(markerY0); // float(markerY0 != CELL_SOLID);
+    numNonSolidNeighbors += abs(markerY1); // float(markerY1 != CELL_SOLID);
+    numNonSolidNeighbors += abs(markerZ0); // float(markerZ0 != CELL_SOLID);
+    numNonSolidNeighbors += abs(markerZ1); // float(markerZ1 != CELL_SOLID);
+
+    // apply diagonal of A
+    if (numNonSolidNeighbors > 0.0) {
+        result += numNonSolidNeighbors * valueAtGridCoord;
+    }
+
+    // apply other coefficients of A
+    if (markerX0 == CELL_FLUID) {
+        result -= texelFetch(texture, gridCoord - ivec3(1, 0, 0), 0).x;
+    }
+    if (markerX1 == CELL_FLUID) {
+        result -= texelFetch(texture, gridCoord + ivec3(1, 0, 0), 0).x;
+    }
+    if (markerY0 == CELL_FLUID) {
+        result -= texelFetch(texture, gridCoord - ivec3(0, 1, 0), 0).x;
+    }
+    if (markerY1 == CELL_FLUID) {
+        result -= texelFetch(texture, gridCoord + ivec3(0, 1, 0), 0).x;
+    }
+    if (markerZ0 == CELL_FLUID) {
+        result -= texelFetch(texture, gridCoord - ivec3(0, 0, 1), 0).x;
+    }
+    if (markerZ1 == CELL_FLUID) {
+        result -= texelFetch(texture, gridCoord + ivec3(0, 0, 1), 0).x;
+    }
+    return result;
+}
