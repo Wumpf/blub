6fb67bb680f83c8e868181b3d63b4743df9da36d
Andreas Reich <r_andreas2@web.de>
2020-10-29 18:28:00 +0100

    SH experimentations

--- shader/background_cubemap.frag
+++ shader/background_cubemap.frag
@@ -1,8 +1,14 @@
 #version 460
 
+//#define VISUALIZE_SH_RADIANCE
+
 #include "per_frame_resources.glsl"
 #include "sky.glsl"
 #include "utilities.glsl"
+
+#ifdef VISUALIZE_SH_RADIANCE
+#include "sh.glsl"
+#endif
 
 layout(location = 0) out vec4 out_Color;
 
@@ -10,6 +16,15 @@ void main() {
     // Too lazy to do this cleaner, also doesn't matter perf wise :)
     vec3 dir = reconstructWorldPositionFromViewSpaceDepth(gl_FragCoord.xy * Screen.ResolutionInv, 1.0) - Camera.Position;
 
-    out_Color.xyz = sampleHdrCubemap(dir);
-    out_Color.w = 1.0;
+    out_Color.rgb = sampleHdrCubemap(dir);
+    out_Color.a = 1.0;
+
+#ifdef VISUALIZE_SH_RADIANCE
+    vec3 shCoeffs[9] =
+        vec3[9](vec3(0.26880783, 0.26525503, 0.25323117), vec3(-0.18061855, -0.182183, -0.17731333), vec3(0.13950694, 0.13673073, 0.12422592),
+                vec3(-0.3194243, -0.31284282, -0.28476116), vec3(0.20926912, 0.20343526, 0.1833044), vec3(-0.09322964, -0.09043422, -0.08140675),
+                vec3(-0.09613961, -0.0930359, -0.08279946), vec3(-0.15792988, -0.15315816, -0.13775906), vec3(0.1190775, 0.11848518, 0.1101599));
+    dir = normalize(dir);
+    out_Color.rgb = sh3Evaluate(dir, shCoeffs);
+#endif
 }
---
+++ shader/sh.glsl
@@ -0,0 +1,95 @@
+// Evalutes SH bands 0,1,2 for a given direction
+
+#define SH_FACTOR_BAND0 0.282094792 // 1.0 / (2.0 * sqrt(PI))
+
+#define SH_FACTOR_BAND1 0.488602512      // sqrt(3.0) / (2.0 * sqrt(PI))
+#define SH_FACTOR_BAND2_non0 1.092548431 // sqrt(15.0) / (2.0 * sqrt(PI))
+#define SH_FACTOR_BAND2_0 0.315391565    // sqrt(5.0) / (4.0 * sqrt(PI))
+
+#define SH_FACTOR_BAND3_0 0.373176336
+#define SH_FACTOR_BAND3_1 0.457045794
+#define SH_FACTOR_BAND3_2 2.89061141
+#define SH_FACTOR_BAND3_3 0.590043604
+
+vec3 sh3Evaluate(vec3 dir, vec3 shCoeffs[9]) {
+    vec3 result = shCoeffs[0] * SH_FACTOR_BAND0;
+    result += shCoeffs[1] * (-SH_FACTOR_BAND1 * dir.y);
+    result += shCoeffs[2] * (SH_FACTOR_BAND1 * dir.z);
+    result += shCoeffs[3] * (-SH_FACTOR_BAND1 * dir.x);
+    result += shCoeffs[4] * (SH_FACTOR_BAND2_non0 * dir.y * dir.x);
+    result += shCoeffs[5] * (-SH_FACTOR_BAND2_non0 * dir.y * dir.z);
+    result += shCoeffs[6] * (SH_FACTOR_BAND2_0 * (3.0 * dir.z * dir.z - 1.0));
+    result += shCoeffs[7] * (-SH_FACTOR_BAND2_non0 * dir.x * dir.z);
+    result += shCoeffs[8] * (SH_FACTOR_BAND2_non0 * 0.5 * (dir.x * dir.x - dir.y * dir.y));
+    return max(vec3(0.0), result);
+}
+
+vec3 sh4Evaluate(vec3 dir, vec3 shCoeffs[16]) {
+    vec3 result = shCoeffs[0] * SH_FACTOR_BAND0;
+
+    result += shCoeffs[1] * (-SH_FACTOR_BAND1 * dir.y);
+    result += shCoeffs[2] * (SH_FACTOR_BAND1 * dir.z);
+    result += shCoeffs[3] * (-SH_FACTOR_BAND1 * dir.x);
+
+    result += shCoeffs[4] * (SH_FACTOR_BAND2_non0 * dir.y * dir.x);
+    result += shCoeffs[5] * (-SH_FACTOR_BAND2_non0 * dir.y * dir.z);
+    result += shCoeffs[6] * (SH_FACTOR_BAND2_0 * (3.0 * dir.z * dir.z - 1.0));
+    result += shCoeffs[7] * (-SH_FACTOR_BAND2_non0 * dir.x * dir.z);
+    result += shCoeffs[8] * (SH_FACTOR_BAND2_non0 * 0.5 * (dir.x * dir.x - dir.y * dir.y));
+
+    result += shCoeffs[9] * (-SH_FACTOR_BAND3_3 * dir.y * (3.0 * dir.x * dir.x - dir.y * dir.y));
+    result += shCoeffs[10] * (SH_FACTOR_BAND3_2 * dir.x * dir.y * dir.z);
+    result += shCoeffs[11] * (-SH_FACTOR_BAND3_1 * dir.y * (5.0 * dir.z * dir.z - 1.0));
+    result += shCoeffs[12] * (SH_FACTOR_BAND3_0 * dir.z * (5.0 * dir.z * dir.z - 3.0));
+    result += shCoeffs[13] * (-SH_FACTOR_BAND3_1 * dir.x * (5.0 * dir.z * dir.z - 1.0));
+    result += shCoeffs[14] * (SH_FACTOR_BAND3_2 * (0.5 * (dir.x * dir.x - dir.y * dir.y) * dir.z));
+    result += shCoeffs[15] * (-SH_FACTOR_BAND3_3 * dir.x * (dir.x * dir.x - 3.0 * dir.y * dir.y));
+
+    return max(vec3(0.0), result);
+}
+// Wasn't able to confirm myself, but several sources state that the (zonal) coefficients for
+// Henyey Greenstein are simply just g^n (n being the band index)
+// See:
+// * http://sjbrown.co.uk/2004/10/16/spherical-harmonic-basis/
+// * https://bartwronski.files.wordpress.com/2014/08/bwronski_volumetric_fog_siggraph2014.pdf
+// * https://books.google.se/books?id=dDL3DwAAQBAJ&pg=PA325&lpg=PA325&dq=henyey+greenstein+zonal+harmonics&source=bl&ots=uN8TFxkSLx&sig=ACfU3U2wZ-poPmmigPUxpDuOaUrchcLxxA&hl=en&sa=X&ved=2ahUKEwjPtLW4yM3sAhXososKHVVeD7kQ6AEwCXoECAgQAg#v=onepage&q=henyey%20greenstein%20zonal%20harmonics&f=false
+vec3 sh3EvaluateHenyeyGreensteinPhaseFunction(vec3 g, vec3 dir, vec3 shCoeffs[9]) {
+    vec3 gSq = sq(g);
+    dir = -dir;
+
+    vec3 result = shCoeffs[0] * SH_FACTOR_BAND0;
+    result += g * shCoeffs[1] * (-SH_FACTOR_BAND1 * dir.y);
+    result += g * shCoeffs[2] * (SH_FACTOR_BAND1 * dir.z);
+    result += g * shCoeffs[3] * (-SH_FACTOR_BAND1 * dir.x);
+    result += gSq * shCoeffs[4] * (SH_FACTOR_BAND2_non0 * dir.y * dir.x);
+    result += gSq * shCoeffs[5] * (-SH_FACTOR_BAND2_non0 * dir.y * dir.z);
+    result += gSq * shCoeffs[6] * (SH_FACTOR_BAND2_0 * (3.0 * dir.z * dir.z - 1.0));
+    result += gSq * shCoeffs[7] * (-SH_FACTOR_BAND2_non0 * dir.x * dir.z);
+    result += gSq * shCoeffs[8] * (SH_FACTOR_BAND2_non0 * 0.5 * (dir.x * dir.x - dir.y * dir.y));
+    return max(vec3(0.0), result);
+}
+
+vec3 sh4EvaluateHenyeyGreensteinPhaseFunction(vec3 g, vec3 dir, vec3 shCoeffs[16]) {
+    vec3 gSq = sq(g);
+    vec3 ggg = gSq * g;
+    dir = -dir;
+
+    vec3 result = shCoeffs[0] * SH_FACTOR_BAND0;
+    result += g * shCoeffs[1] * (-SH_FACTOR_BAND1 * dir.y);
+    result += g * shCoeffs[2] * (SH_FACTOR_BAND1 * dir.z);
+    result += g * shCoeffs[3] * (-SH_FACTOR_BAND1 * dir.x);
+    result += gSq * shCoeffs[4] * (SH_FACTOR_BAND2_non0 * dir.y * dir.x);
+    result += gSq * shCoeffs[5] * (-SH_FACTOR_BAND2_non0 * dir.y * dir.z);
+    result += gSq * shCoeffs[6] * (SH_FACTOR_BAND2_0 * (3.0 * dir.z * dir.z - 1.0));
+    result += gSq * shCoeffs[7] * (-SH_FACTOR_BAND2_non0 * dir.x * dir.z);
+    result += gSq * shCoeffs[8] * (SH_FACTOR_BAND2_non0 * 0.5 * (dir.x * dir.x - dir.y * dir.y));
+    result += ggg * shCoeffs[9] * (-SH_FACTOR_BAND3_3 * dir.y * (3.0 * dir.x * dir.x - dir.y * dir.y));
+    result += ggg * shCoeffs[10] * (SH_FACTOR_BAND3_2 * dir.x * dir.y * dir.z);
+    result += ggg * shCoeffs[11] * (-SH_FACTOR_BAND3_1 * dir.y * (5.0 * dir.z * dir.z - 1.0));
+    result += ggg * shCoeffs[12] * (SH_FACTOR_BAND3_0 * dir.z * (5.0 * dir.z * dir.z - 3.0));
+    result += ggg * shCoeffs[13] * (-SH_FACTOR_BAND3_1 * dir.x * (5.0 * dir.z * dir.z - 1.0));
+    result += ggg * shCoeffs[14] * (SH_FACTOR_BAND3_2 * (0.5 * (dir.x * dir.x - dir.y * dir.y) * dir.z));
+    result += ggg * shCoeffs[15] * (-SH_FACTOR_BAND3_3 * dir.x * (dir.x * dir.x - 3.0 * dir.y * dir.y));
+
+    return max(vec3(0.0), result);
+}
